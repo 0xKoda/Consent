fb75a4395f2759f91393a3c9d20ae073f0534d90
defobari <brownjab21@gmail.com>
2022-06-01 11:19:09 -0500

    Smol fixes

--- src/Consent.sol
+++ src/Consent.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.13;
 
-contract Playground {
+contract Consent {
 
     mapping(address => mapping(address => bool)) public userConsent;
 
